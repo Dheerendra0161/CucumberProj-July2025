3559b31235acc1b8150a6aecfa459ec5ae2e5db2
Dheerendra <dheerendra0161@gmail.com>
2025-07-11 18:44:06 +0530

    Initial commit - added base framework

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,10 +1,17 @@
 package hooks;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import base.BaseTest;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
-
+import io.cucumber.java.Scenario;
 public class Hooks {
 
     BaseTest baseTest = new BaseTest();
@@ -16,7 +23,24 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
-        baseTest.tearDown();
-    }
-}
+    public void tearDown(Scenario scenario) {
+        if (driver != null) {
+            if (scenario.isFailed()) {
+                // Take screenshot only if driver is initialized and test failed
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                File source = ts.getScreenshotAs(OutputType.FILE);
+                String fileName = "screenshot_" + System.currentTimeMillis() + ".png";
+                File destination = new File("screenshots/" + fileName);
+
+                try {
+                    FileUtils.copyFile(source, destination);
+                    System.out.println("Screenshot saved: " + destination.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            driver.quit(); // always close the browser
+        } else {
+            System.out.println("WebDriver was null in @After hook.");
+        }
+    }}
